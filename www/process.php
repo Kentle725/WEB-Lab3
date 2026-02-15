6c10523d9f1b64aa23472a7f2f7658645dd93260
Kentle725 <vova34k@mail.ru>
2026-02-15 17:01:04 +0200

    Added process.php

---
+++ www/process.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+$patient_name = htmlspecialchars($_POST['patient_name']);
+$age = htmlspecialchars($_POST['age']);
+$doctor = htmlspecialchars($_POST['doctor']);
+$first_consultation = htmlspecialchars($_POST['first_consultation'] ?? FALSE);
+$visit_form = htmlspecialchars($_POST['visit_form']);
+
+$_SESSION['patient_name'] = $patient_name;
+$_SESSION['age'] = $age;
+$_SESSION['doctor'] = $doctor;
+$_SESSION['first_consultation'] = $first_consultation;
+$_SESSION['visit_form'] = $visit_form;
+
+header("Location: index.php");
+exit();
